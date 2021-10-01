5e31d6a3a3e6ad6b3d06e440c75eff34d3ab32a7
mjb4040 <michaeljamesblackwell.@gmail.com>
2021-09-30 20:13:24 -0400

    git stash to mjb

--- assets/script.js
+++ assets/script.js
@@ -1,10 +1,14 @@
-fetch(
-    'https://api.giphy.com/v1/gifs/trending?api_key=HvaacROi9w5oQCDYHSIk42eiDSIXH3FN'
-  )
-    .then(function(response) {
-      return response.json();
-    })
-    .then(function(data) {
-      console.log(data);
-    });
-  
+fetch("https://giphy.p.rapidapi.com/v1/gifs/search?api_key=dc6zaTOxFJmzC&q=drinks", {
+	"method": "GET",
+	"headers": {
+		"x-rapidapi-host": "giphy.p.rapidapi.com",
+		"x-rapidapi-key": "ebad596933msh0bd0d60b9facb1bp1906dejsn061864291ea2"
+	}
+})
+.then(response => {
+	console.log(response);
+})
+.catch(err => {
+	console.error(err);
+});
+
