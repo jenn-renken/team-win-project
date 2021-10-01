f86bbf0253518a51788b316058abe28ab7f89869
jenn-renken <88343948+jenn-renken@users.noreply.github.com>
2021-10-01 14:21:14 -0400

    update javascript

--- assets/script.js
+++ assets/script.js
@@ -1,112 +1,112 @@
 // Array of questions for the quiz.
-// var questions = [
-//     { 
-//         question: 'Who invented JavaScript?', 
-//         choices: ['Elon Musk', 'Steve Jobs', 'Bill Gates', 'Brendan Eich'],
-//         answer: 'Brendan Eich' 
-//     },
-//     {
-//         question: "String values must be enclosed within ____ when being assigned to variables.",
-//         choices: ["commas", "curly brackets", "quotes", "parenthesis"],
-//         answer: "quotes"
-//     },
-//     {
-//         question: "A useful tool to debug code and print content is:",
-//         choices: ["for()", "document.getElement", ".addEventListener", "console.log()"],
-//         answer: "console.log()"
-//     },
-//     {    
-//         question: "Which of the following is a data type?",
-//         choices: ["prompt", "boolean", "alert", "variable"],
-//         answer: "boolean"
-//     },
-//     {
-//         question: "What is an array used for?",
-//         choices: ["storing numbers and strings", "iterating through data", "displaying content", "none of the above"],
-//         answer: "storing numbers and strings"
-//     },
-//   ];
+var questions = [
+    { 
+        question: 'Who invented JavaScript?', 
+        choices: ['Elon Musk', 'Steve Jobs', 'Bill Gates', 'Brendan Eich'],
+        answer: 'Brendan Eich' 
+    },
+    {
+        question: "String values must be enclosed within ____ when being assigned to variables.",
+        choices: ["commas", "curly brackets", "quotes", "parenthesis"],
+        answer: "quotes"
+    },
+    {
+        question: "A useful tool to debug code and print content is:",
+        choices: ["for()", "document.getElement", ".addEventListener", "console.log()"],
+        answer: "console.log()"
+    },
+    {    
+        question: "Which of the following is a data type?",
+        choices: ["prompt", "boolean", "alert", "variable"],
+        answer: "boolean"
+    },
+    {
+        question: "What is an array used for?",
+        choices: ["storing numbers and strings", "iterating through data", "displaying content", "none of the above"],
+        answer: "storing numbers and strings"
+    },
+  ];
 
-// var score = 0;
-// var questionIndex = 0;
-// var holdInterval = 0;
+var score = 0;
+var questionIndex = 0;
+var holdInterval = 0;
 
 
-// var hideResponseTimeout = null;
+var hideResponseTimeout = null;
 
-//   var elements = {
-//      startQuiz: document.querySelector("#startQuiz"),
-//      wrapper: document.querySelector("#wrapper"),
-//      question: document.querySelector("#question"),
-//      questionChoice: document.querySelector("#questionChoice"),
-//      intro: document.querySelector("#intro"),
-//      questionTitle: document.querySelector("#questionTitle"),
-//      viewHighScores: document.querySelector("#viewHighScores"),
-//      scores: document.querySelector("#scores"),
-//      highScore: document.querySelector("#highScore"),
-//      clear: document.querySelector("#clear"),
-//      goBack: document.querySelector("#goBack"),
-//      finalScore: document.querySelector("#finalScore"),
-//   };
+  var elements = {
+     startQuiz: document.querySelector("#startQuiz"),
+     wrapper: document.querySelector("#wrapper"),
+     question: document.querySelector("#question"),
+     questionChoice: document.querySelector("#questionChoice"),
+     intro: document.querySelector("#intro"),
+     questionTitle: document.querySelector("#questionTitle"),
+     viewHighScores: document.querySelector("#viewHighScores"),
+     scores: document.querySelector("#scores"),
+     highScore: document.querySelector("#highScore"),
+     clear: document.querySelector("#clear"),
+     goBack: document.querySelector("#goBack"),
+     finalScore: document.querySelector("#finalScore"),
+  };
 
-// function showWrapperElement(element) {
-//     var children = elements.wrapper.children;
-//     for (var i = 0; i < children.length; i++) {
-//         var child = children[i];
-//         hideElement(child);
-//     }
-//     showElement(elements.wrapper);
-//     if (!element) {
-//         return;
-//     }
-//     showElement(element);
-// }
+function showWrapperElement(element) {
+    var children = elements.wrapper.children;
+    for (var i = 0; i < children.length; i++) {
+        var child = children[i];
+        hideElement(child);
+    }
+    showElement(elements.wrapper);
+    if (!element) {
+        return;
+    }
+    showElement(element);
+}
 
-// function showElement(element) {
-//     element.style.display = "";
-// }
+function showElement(element) {
+    element.style.display = "";
+}
 
-// function hideElement(element) {
-//     element.style.display = "none";
-// }
+function hideElement(element) {
+    element.style.display = "none";
+}
 
-// function startQuiz() {
-//     startTimer();
-//     showQuizItem(0);
+function startQuiz() {
+    // startTimer();
+    showQuizItem(0);
     
-// }
+}
 
 // function hideResponse () {
 //     hideElement(elements.correct);
 //     hideElement(elements.wrong);
 // }
 
-// function showQuizItem(number) {
-//     showWrapperElement();
-//     var delay = number? 2000:0;
-//     hideResponseTimeout = setTimeout(hideResponse, delay);
-//     var question = questions[number];
-//     elements.questionTitle.innerHTML = question.question;
-//     showChoices(number);
-//     showWrapperElement(elements.question);
-// }
+function showQuizItem(number) {
+    showWrapperElement();
+    var delay = number? 2000:0;
+    hideResponseTimeout = setTimeout(hideResponse, delay);
+    var question = questions[number];
+    elements.questionTitle.innerHTML = question.question;
+    showChoices(number);
+    showWrapperElement(elements.question);
+}
 
-// function showChoices(number) {
-//     var choices = questions[number].choices;
-//     removeAllChildNodes(elements.questionChoice);
-//     choices.forEach(function (newItem) {
-//         var listItem = document.createElement("li");
-//         listItem.textContent = newItem;
-//         elements.questionChoice.appendChild(listItem);
-//         listItem.addEventListener("click", onChoice);
-//     })
-// };
+function showChoices(number) {
+    var choices = questions[number].choices;
+    removeAllChildNodes(elements.questionChoice);
+    choices.forEach(function (newItem) {
+        var listItem = document.createElement("li");
+        listItem.textContent = newItem;
+        elements.questionChoice.appendChild(listItem);
+        listItem.addEventListener("click", onChoice);
+    })
+};
 
-// function removeAllChildNodes(parent) {
-//     while (parent.firstChild) {
-//         parent.removeChild(parent.firstChild);
-//     }
-// }
+function removeAllChildNodes(parent) {
+    while (parent.firstChild) {
+        parent.removeChild(parent.firstChild);
+    }
+}
 
 // // compare choices with answer
 // function onChoice(event) {
@@ -196,21 +196,21 @@
 //     showScores();
 // });
 
-// elements.goBack.addEventListener("click", function () {
-//      score = 0;
-//      questionIndex = 0;
-//      secondsLeft = questions.length * 20;
-//      holdInterval = 0;
-//      elements.currentTime.textContent = "";
-//      elements.inputInitials.value = "";
-//     showWrapperElement(elements.intro);
+elements.goBack.addEventListener("click", function () {
+     score = 0;
+     questionIndex = 0;
+     secondsLeft = questions.length * 20;
+     holdInterval = 0;
+     elements.currentTime.textContent = "";
+     elements.inputInitials.value = "";
+    showWrapperElement(elements.intro);
     
-// });
+});
 
 // elements.viewHighScores.addEventListener("click", showScores);
 
-// showWrapperElement(elements.intro);
-// elements.startQuiz.addEventListener("click", startQuiz);
+showWrapperElement(elements.intro);
+elements.startQuiz.addEventListener("click", startQuiz);
 
     
 // "margarita" can be replaced with our var of choice 
